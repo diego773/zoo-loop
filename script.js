f550003d8d66ac81221a62194793279338ebe8e9
Diego <diegovillarreal123@gmail.com>
2021-02-22 13:44:11 -0600

    practizing javascript loops

---
+++ script.js
@@ -0,0 +1,15 @@
+// Create an array of animals
+var animals = ["Zebra", "Rhino", "Giraffe", "Owl", "Dog"];
+
+//Console log animals
+// console.log(animals[0]);
+// console.log(animals[1]);
+// console.log(animals[2]);
+// console.log(animals[3]);
+
+// var lenght = animals.length
+// console.log(lenght);
+
+for (var i=0; i<animals.length; i++) {
+    console.log(animals[i]);
+}
